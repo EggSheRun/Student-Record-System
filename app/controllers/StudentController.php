9d878f43c1372caa4b7504d3d5cc4adc67609993
EggSheRun <rhayabusa960@gmail.com>
2026-03-24 18:04:07 +0800

    Update StudentController

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -16,14 +16,43 @@ class StudentController {
     }
 
     public function create() {
-        $create = $this->model();
+        
+    require_once __DIR__ . '/../views/students/create.php';
     }
 
     public function store() {
-        $store = $this->model->createStudent($data);
-
-        require '../views/students/create.php';
+        $data = [
+            'name' => $_POST['name'],
+            'email' => $_POST['email'],
+            'course' => $_POST['course'],
+            'year_level' => $_POST['year_level'],
+            'status' => $_POST['status'],
+        ];
+        $this->model->createStudent($data);
+        header('location: ../index.php');
     }
 
-    public function
+    public function edit($id) {
+        $students = $this->model->getStudentById($id);
+        require __DIR__ . '/../views/students/edit.php';
+    }
+
+    public function update($id) {
+        $data = [
+            'id' => $id,
+            'name' => $_POST['name'],
+            'email' => $_POST['email'],
+            'course' => $_POST['course'],
+            'year_level' => $_POST['year_level'],
+            'status' => $_POST['status'],
+            ];
+            $this->model->updateStudent($data);
+            header('location: ../index.php');
+
+    }
+
+    public function delete($id) {
+        $this->model->deleteStudent($id);
+        header('location: ../index.php');
+    }
 }
